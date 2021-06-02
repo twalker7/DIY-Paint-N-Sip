d1411958e1cc30686a1c891d330b926a8996dd35
Thomas Walker <thomasnwalker@yahoo.com>
2021-06-01 18:11:50 -0700

    draft media queries created, JS fetch functions drafted

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,40 @@
+// functions containing fetch requests -- not currently working properly; just for testing purposes
 
-// this button will call the function containing a fetch request when to the cocktail API when pressed -- for now, holding an alert() for testing
-$("#cocktail-button").click(function(){
-   // getDrink();
-    alert("cocktail button pressed");
+var getDrink = function(){
+    
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + $("#cocktail-submit").val() )
+    .then(response=>response.json())
+    .then(function(data){
+        console.log(data);
+        console.log(data.drinks[4].strInstructions);
+        alert("request test - return ingredients: \n" + data.drinks[4].strInstructions);
+    });
+    
+
+}
+
+var getArtwork = function(){
+    fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1")
+    .then(response=>response.json())
+    .then(function(obj){
+        console.log(obj);
+        console.log(obj.objectIDs[3]);
+        
+    });
+}
+
+$("#cocktail-button").click(function(event){
+    event.preventDefault();
+    getDrink();
+   //alert("cocktail gbutton pressed");
 });
 
 //to call the function containing a fetch request to art museum API 
 $("#new-art-button").click(function(){
     alert("new art button pressed");
+    getArtwork();
 });
 
 
 
-/* 
 
-function to send fetch request goes below  
-var getDrink = function(){
-    fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=" + $("").val() );
-}
-
-*/
